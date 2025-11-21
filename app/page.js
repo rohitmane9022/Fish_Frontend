59cbb4d23e8c35de918b29a1662f7d3cd9575146
rohitmane9022 <rohitmane8356@gmail.com>
2025-11-22 02:02:41 +0530

    Update App Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
-import MainPage from "../components/MainPage"
+import MainPage from "../components/MainPage";
+
+export const metadata = {
+  title: "Bombay Sea Food – Fresh & Clean Seafood Delivery in Mumbai",
+  description:
+    "Buy fresh fish, prawns, chicken, and seafood online with same-day delivery across Mumbai. Bombay Sea Food provides premium, hygienic, and high-quality seafood sourced directly from trusted suppliers.",
+};
 
 export default function Home() {
-  
-
   return (
     <div className="">
-    <MainPage/>
+      <MainPage />
     </div>
   );
 }
